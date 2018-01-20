1324600c48a91cd4e51ce39a98a395ef8704f4e7
nishanth <nishanthalladi01@gmail.com>
2018-01-20 14:46:33 -0500

    come my child

--- app/src/main/java/com/example/evanphoward/hackbi/MainActivity.java
+++ app/src/main/java/com/example/evanphoward/hackbi/MainActivity.java
@@ -1,82 +1,107 @@
 package com.example.evanphoward.hackbi;
 
+import android.app.ActivityManager;
 import android.content.Context;
+import android.content.pm.ConfigurationInfo;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class MainActivity extends AppCompatActivity {
-
-    private GLSurfaceView mGLView;
+//
+//    private GLSurfaceView mGLView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        mGLView = new MyGLSurfaceView(this);
+//        setContentView(mGLView);
+//    }
+//
+//    private class MyGLSurfaceView extends GLSurfaceView {
+//        private final MyGLRenderer mRenderer;
+//
+//        public MyGLSurfaceView(Context context) {
+//            super(context);
+//
+//            // Create an OpenGL ES 2.0 context.
+//            setEGLContextClientVersion(2);
+//
+//            // Set the Renderer for drawing on the GLSurfaceView
+//            mRenderer = new MyGLRenderer(getApplicationContext());
+//            setRenderer(mRenderer);
+//
+//            // Render the view only when there is a change in the drawing data
+//            setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+//        }
+//
+//        private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+//        private float mPreviousX;
+//        private float mPreviousY;
+//
+//        @Override
+//        public boolean onTouchEvent(MotionEvent e) {
+//            // MotionEvent reports input details from the touch screen
+//            // and other input controls. In this case, you are only
+//            // interested in events where the touch position changed.
+//
+//            float x = e.getX();
+//            float y = e.getY();
+//
+//            switch (e.getAction()) {
+//                case MotionEvent.ACTION_MOVE:
+//
+//                    float dx = x - mPreviousX;
+//                    float dy = y - mPreviousY;
+//
+//                    // reverse direction of rotation above the mid-line
+//                    if (y > getHeight() / 2) {
+//                        dx = dx * -1 ;
+//                    }
+//
+//                    // reverse direction of rotation to left of the mid-line
+//                    if (x < getWidth() / 2) {
+//                        dy = dy * -1 ;
+//                    }
+//
+//                    mRenderer.setAngle(
+//                            mRenderer.getAngle() +
+//                                    ((dx + dy) * TOUCH_SCALE_FACTOR));  // = 180.0f / 320
+//                    requestRender();
+//            }
+//
+//            mPreviousX = x;
+//            mPreviousY = y;
+//            return true;
+//        }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (detectOpenGLES30()) {
+            //so we know it a opengl 3.0 and use our extended GLsurfaceview.
+            setContentView(new myGLSurfaceView(this));
+        } else {
+            // This is where you could create an OpenGL ES 2.0 and/or 1.x compatible
+            // renderer if you wanted to support both ES 1 and ES 2.
+            Log.e("openglcube", "OpenGL ES 3.0 not supported on device.  Exiting...");
+            finish();
 
-        mGLView = new MyGLSurfaceView(this);
-        setContentView(mGLView);
+        }
+
     }
-
-    private class MyGLSurfaceView extends GLSurfaceView {
-        private final MyGLRenderer mRenderer;
-
-        public MyGLSurfaceView(Context context) {
-            super(context);
-
-            // Create an OpenGL ES 2.0 context.
-            setEGLContextClientVersion(2);
-
-            // Set the Renderer for drawing on the GLSurfaceView
-            mRenderer = new MyGLRenderer();
-            setRenderer(mRenderer);
-
-            // Render the view only when there is a change in the drawing data
-            setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-        }
-
-        private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
-        private float mPreviousX;
-        private float mPreviousY;
-
-        @Override
-        public boolean onTouchEvent(MotionEvent e) {
-            // MotionEvent reports input details from the touch screen
-            // and other input controls. In this case, you are only
-            // interested in events where the touch position changed.
-
-            float x = e.getX();
-            float y = e.getY();
-
-            switch (e.getAction()) {
-                case MotionEvent.ACTION_MOVE:
-
-                    float dx = x - mPreviousX;
-                    float dy = y - mPreviousY;
-
-                    // reverse direction of rotation above the mid-line
-                    if (y > getHeight() / 2) {
-                        dx = dx * -1 ;
-                    }
-
-                    // reverse direction of rotation to left of the mid-line
-                    if (x < getWidth() / 2) {
-                        dy = dy * -1 ;
-                    }
-
-                    mRenderer.setAngle(
-                            mRenderer.getAngle() +
-                                    ((dx + dy) * TOUCH_SCALE_FACTOR));  // = 180.0f / 320
-                    requestRender();
-            }
-
-            mPreviousX = x;
-            mPreviousY = y;
-            return true;
-        }
+    private boolean detectOpenGLES30() {
+        ActivityManager am =
+                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo info = am.getDeviceConfigurationInfo();
+        return (info.reqGlEsVersion >= 0x30000);
     }
 }
--- app/src/main/java/com/example/evanphoward/hackbi/MyGLRenderer.java
+++ app/src/main/java/com/example/evanphoward/hackbi/MyGLRenderer.java
@@ -1,6 +1,8 @@
 package com.example.evanphoward.hackbi;
 
+import android.content.Context;
 import android.opengl.GLES20;
+import android.opengl.GLES30;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
 import android.os.SystemClock;
@@ -15,9 +17,144 @@ import javax.microedition.khronos.opengles.GL10;
 
 public class MyGLRenderer implements GLSurfaceView.Renderer {
 
-    private static final String TAG = "MyGLRenderer";
-    private Triangle mTriangle;
-    private Square   mSquare;
+//    private static final String TAG = "MyGLRenderer";
+//    private Triangle mTriangle;
+//    //private Square   mSquare;
+//
+//    // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+//    private final float[] mMVPMatrix = new float[16];
+//    private final float[] mProjectionMatrix = new float[16];
+//    private final float[] mViewMatrix = new float[16];
+//    private final float[] mRotationMatrix = new float[16];
+//
+//    private float mAngle;
+//
+//    @Override
+//    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+//
+//        // Set the background frame color
+//        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+//
+//        mTriangle = new Triangle();
+//        //mSquare   = new Square();
+//    }
+//
+//    @Override
+//    public void onDrawFrame(GL10 unused) {
+//        float[] scratch = new float[16];
+//
+//        // Draw background color
+//        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+//
+//        // Set the camera position (View matrix)
+//        Matrix.setLookAtM(mViewMatrix, 0, 1, 2, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+//
+//        // Calculate the projection and view transformation
+//        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+//
+//        // Draw square
+//       // mSquare.draw(mMVPMatrix);
+//
+//        // Create a rotation for the triangle
+//
+//        // Use the following code to generate constant rotation.
+//        // Leave this code out when using TouchEvents.
+//         //long time = SystemClock.uptimeMillis() % 4000L;
+//         //float angle = 0.090f * ((int) time);
+//
+//        Matrix.setRotateM(mRotationMatrix, 0, mAngle, 0, 0, 1.0f);
+//
+//        // Combine the rotation matrix with the projection and camera view
+//        // Note that the mMVPMatrix factor *must be first* in order
+//        // for the matrix multiplication product to be correct.
+//        Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
+//
+//        // Draw triangle
+//        mTriangle.draw(scratch);
+//    }
+//
+//    @Override
+//    public void onSurfaceChanged(GL10 unused, int width, int height) {
+//        // Adjust the viewport based on geometry changes,
+//        // such as screen rotation
+//        GLES20.glViewport(0, 0, width, height);
+//
+//        float ratio = (float) width / height;
+//
+//        // this projection matrix is applied to object coordinates
+//        // in the onDrawFrame() method
+//        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+//
+//    }
+//
+//    /**
+//     * Utility method for compiling a OpenGL shader.
+//     *
+//     * <p><strong>Note:</strong> When developing shaders, use the checkGlError()
+//     * method to debug shader coding errors.</p>
+//     *
+//     * @param type - Vertex or fragment shader type.
+//     * @param shaderCode - String containing the shader code.
+//     * @return - Returns an id for the shader.
+//     */
+//    public static int loadShader(int type, String shaderCode){
+//
+//        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+//        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+//        int shader = GLES20.glCreateShader(type);
+//
+//        // add the source code to the shader and compile it
+//        GLES20.glShaderSource(shader, shaderCode);
+//        GLES20.glCompileShader(shader);
+//
+//        return shader;
+//    }
+//
+//    /**
+//     * Utility method for debugging OpenGL calls. Provide the name of the call
+//     * just after making it:
+//     *
+//     * <pre>
+//     * mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+//     * MyGLRenderer.checkGlError("glGetUniformLocation");</pre>
+//     *
+//     * If the operation is not successful, the check throws an error.
+//     *
+//     * @param glOperation - Name of the OpenGL call to check.
+//     */
+//    public static void checkGlError(String glOperation) {
+//        int error;
+//        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+//            Log.e(TAG, glOperation + ": glError " + error);
+//            throw new RuntimeException(glOperation + ": glError " + error);
+//        }
+//    }
+//
+//    /**
+//     * Returns the rotation angle of the triangle shape (mTriangle).
+//     *
+//     * @return - A float representing the rotation angle.
+//     */
+//    public float getAngle() {
+//        return mAngle;
+//    }
+//
+//    /**
+//     * Sets the rotation angle of the triangle shape (mTriangle).
+//     */
+//    public void setAngle(float angle) {
+//        mAngle = angle;
+//    }
+
+    private int mWidth;
+    private int mHeight;
+    private static String TAG = "myRenderer";
+    public Pyramid mPyramid;
+    private float mAngle =0;
+    private float mTransY=0;
+    private float mTransX=0;
+    private static final float Z_NEAR = 1f;
+    private static final float Z_FAR = 40f;
 
     // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
     private final float[] mMVPMatrix = new float[16];
@@ -25,85 +162,41 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     private final float[] mViewMatrix = new float[16];
     private final float[] mRotationMatrix = new float[16];
 
-    private float mAngle;
-
-    @Override
-    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
-
-        // Set the background frame color
-        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-
-        mTriangle = new Triangle();
-        mSquare   = new Square();
+    //
+    public MyGLRenderer(Context context) {
+        //cube can not be instianated here, because of "no egl context"  no clue.
+        //do it in onSurfaceCreate and it is fine.  odd, but workable solution.
     }
+    ///
+    // Create a shader object, load the shader source, and
+    // compile the shader.
+    //
+    public static int LoadShader(int type, String shaderSrc) {
+        int shader;
+        int[] compiled = new int[1];
 
-    @Override
-    public void onDrawFrame(GL10 unused) {
-        float[] scratch = new float[16];
+        // Create the shader object
+        shader = GLES30.glCreateShader(type);
 
-        // Draw background color
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+        if (shader == 0) {
+            return 0;
+        }
 
-        // Set the camera position (View matrix)
-        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+        // Load the shader source
+        GLES30.glShaderSource(shader, shaderSrc);
 
-        // Calculate the projection and view transformation
-        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+        // Compile the shader
+        GLES30.glCompileShader(shader);
 
-        // Draw square
-       // mSquare.draw(mMVPMatrix);
+        // Check the compile status
+        GLES30.glGetShaderiv(shader, GLES30.GL_COMPILE_STATUS, compiled, 0);
 
-        // Create a rotation for the triangle
-
-        // Use the following code to generate constant rotation.
-        // Leave this code out when using TouchEvents.
-         //long time = SystemClock.uptimeMillis() % 4000L;
-         //float angle = 0.090f * ((int) time);
-
-        Matrix.setRotateM(mRotationMatrix, 0, mAngle, 0, 0, 1.0f);
-
-        // Combine the rotation matrix with the projection and camera view
-        // Note that the mMVPMatrix factor *must be first* in order
-        // for the matrix multiplication product to be correct.
-        Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
-
-        // Draw triangle
-        mTriangle.draw(scratch);
-    }
-
-    @Override
-    public void onSurfaceChanged(GL10 unused, int width, int height) {
-        // Adjust the viewport based on geometry changes,
-        // such as screen rotation
-        GLES20.glViewport(0, 0, width, height);
-
-        float ratio = (float) width / height;
-
-        // this projection matrix is applied to object coordinates
-        // in the onDrawFrame() method
-        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
-
-    }
-
-    /**
-     * Utility method for compiling a OpenGL shader.
-     *
-     * <p><strong>Note:</strong> When developing shaders, use the checkGlError()
-     * method to debug shader coding errors.</p>
-     *
-     * @param type - Vertex or fragment shader type.
-     * @param shaderCode - String containing the shader code.
-     * @return - Returns an id for the shader.
-     */
-    public static int loadShader(int type, String shaderCode){
-
-        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
-        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
-        int shader = GLES20.glCreateShader(type);
-
-        // add the source code to the shader and compile it
-        GLES20.glShaderSource(shader, shaderCode);
-        GLES20.glCompileShader(shader);
+        if (compiled[0] == 0) {
+            Log.e(TAG, "Erorr!!!!");
+            Log.e(TAG, GLES30.glGetShaderInfoLog(shader));
+            GLES30.glDeleteShader(shader);
+            return 0;
+        }
 
         return shader;
     }
@@ -113,7 +206,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
      * just after making it:
      *
      * <pre>
-     * mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+     * mColorHandle = GLES30.glGetUniformLocation(mProgram, "vColor");
      * MyGLRenderer.checkGlError("glGetUniformLocation");</pre>
      *
      * If the operation is not successful, the check throws an error.
@@ -122,25 +215,84 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
      */
     public static void checkGlError(String glOperation) {
         int error;
-        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+        while ((error = GLES30.glGetError()) != GLES30.GL_NO_ERROR) {
             Log.e(TAG, glOperation + ": glError " + error);
             throw new RuntimeException(glOperation + ": glError " + error);
         }
     }
 
-    /**
-     * Returns the rotation angle of the triangle shape (mTriangle).
-     *
-     * @return - A float representing the rotation angle.
-     */
-    public float getAngle() {
-        return mAngle;
+    ///
+    // Initialize the shader and program object
+    //
+    @Override
+    public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
+        //set the clear buffer color to light gray.
+        GLES30.glClearColor(0.9f, .9f, 0.9f, 0.9f);
+        //initialize the cube code for drawing.
+        mPyramid = new Pyramid();
+        //if we had other objects setup them up here as well.
     }
 
-    /**
-     * Sets the rotation angle of the triangle shape (mTriangle).
-     */
-    public void setAngle(float angle) {
-        mAngle = angle;
+    @Override
+    public void onSurfaceChanged(GL10 glUnused, int width, int height) {
+        mWidth = width;
+        mHeight = height;
+        // Set the viewport
+        GLES30.glViewport(0, 0, mWidth, mHeight);
+        float aspect = (float) width / height;
+
+        // this projection matrix is applied to object coordinates
+        //no idea why 53.13f, it was used in another example and it worked.
+        Matrix.perspectiveM(mProjectionMatrix, 0, 53.13f, aspect, Z_NEAR, Z_FAR);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 glUnused) {
+        // Clear the color buffer  set above by glClearColor.
+        GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT | GLES30.GL_DEPTH_BUFFER_BIT);
+
+        //need this otherwise, it will over right stuff and the cube will look wrong!
+        GLES30.glEnable(GLES30.GL_DEPTH_TEST);
+
+        // Set the camera position (View matrix)  note Matrix is an include, not a declared method.
+        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+        // Create a rotation and translation for the cube
+        Matrix.setIdentityM(mRotationMatrix, 0);
+
+        //move the cube up/down and left/right
+        Matrix.translateM(mRotationMatrix, 0, mTransX, mTransY, 0);
+
+        //mangle is how fast, x,y,z which directions it rotates.
+        Matrix.rotateM(mRotationMatrix, 0, mAngle, 0.4f, 1.0f, 0.6f);
+
+        // combine the model with the view matrix
+        Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mRotationMatrix, 0);
+
+        // combine the model-view with the projection matrix
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mMVPMatrix, 0);
+
+        mPyramid.draw(mMVPMatrix);
+
+        //change the angle, so the cube will spin.
+        mAngle+=.4;
+    }
+
+
+    //used the touch listener to move the cube up/down (y) and left/right (x)
+    public float getY() {
+        return mTransY;
+    }
+
+    public void setY(float mY) {
+        mTransY = mY;
+    }
+
+    public float getX() {
+        return mTransX;
+    }
+
+    public void setX(float mX) {
+        mTransX = mX;
     }
 }
