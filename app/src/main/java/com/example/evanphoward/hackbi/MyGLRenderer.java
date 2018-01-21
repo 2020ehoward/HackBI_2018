dd660be6c2c2bac0ec765171aff5e0c854f434f4
Evan Howard <2020ehoward@tjhsst.edu>
2018-01-20 19:35:15 -0500

    Better gyroscope that doesn't really work

--- app/src/main/java/com/example/evanphoward/hackbi/MyGLRenderer.java
+++ app/src/main/java/com/example/evanphoward/hackbi/MyGLRenderer.java
@@ -25,9 +25,9 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     private float mTransY=0;
     private float mTransX=0;
     private float mTransZ=0;
-    private float rotx=0;
-    private float roty=0;
-    private float rotz=0;
+    private float rotx=0.4f;
+    private float roty=1.0f;
+    private float rotz=0.6f;
     private static final float Z_NEAR = 1f;
     private static final float Z_FAR = 40f;
 
@@ -36,8 +36,6 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     private final float[] mProjectionMatrix = new float[16];
     private final float[] mViewMatrix = new float[16];
     private final float[] mRotationMatrix = new float[16];
-    private SensorManager mSensorManager;
-    private Sensor mSensor;
     //
     Context context;
     public MyGLRenderer(Context context) {
@@ -143,7 +141,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         Matrix.translateM(mRotationMatrix, 0, mTransX, mTransY, mTransZ);
 
         //mangle is how fast, x,y,z which directions it rotates.
-        Matrix.rotateM(mRotationMatrix, 0, mAngle, rotx, roty, rotz);
+        Matrix.rotateM(mRotationMatrix, 0, mAngle, rotx,roty,rotz);
 
         // combine the model with the view matrix
         Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mRotationMatrix, 0);
@@ -155,7 +153,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
 
 
         //change the angle, so the cube will spin.
-        mAngle+=.4;
+        //mAngle+=.4;
     }
 
     public float getY() {
--- app/src/main/java/com/example/evanphoward/hackbi/myGLSurfaceView.java
+++ app/src/main/java/com/example/evanphoward/hackbi/myGLSurfaceView.java
@@ -7,7 +7,6 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.view.MotionEvent;
-import android.widget.Toast;
 
 import static android.content.Context.SENSOR_SERVICE;
 
@@ -16,11 +15,33 @@ import static android.content.Context.SENSOR_SERVICE;
  */
 public class myGLSurfaceView extends GLSurfaceView {
 
-    private SensorEventListener sensorEventListener;
     MyGLRenderer myRender;
-    private SensorManager sensorManager;
-    private Sensor sensor;
+    private SensorManager mSensorManager;
+    private final float[] mAccelerometerReading = new float[3];
+    private final float[] mMagnetometerReading = new float[3];
 
+    private final float[] mRotationMatrix = new float[9];
+    private final float[] mOrientationAngles = new float[3];
+
+    private SensorEventListener mSensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor == mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)) {
+                System.arraycopy(event.values, 0, mAccelerometerReading,
+                        0, mAccelerometerReading.length);
+            }
+            else if (event.sensor == mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)) {
+                System.arraycopy(event.values, 0, mMagnetometerReading,
+                        0, mMagnetometerReading.length);
+            }
+            updateOrientationAngles();
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 
     public myGLSurfaceView(Context context) {
         super(context);
@@ -35,39 +56,57 @@ public class myGLSurfaceView extends GLSurfaceView {
         // Render the view only when there is a change in the drawing data
         setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
 
-        sensorManager = (SensorManager) context.getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager=(SensorManager)context.getSystemService(SENSOR_SERVICE);
 
-        sensorEventListener = new SensorEventListener() {
-            float vx=0.5f,vy=0.5f,vz=0.5f;
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-                if(event.values[0]>0.1 || event.values[1]>0.1 || event.values[2]>0.1) {
-                    vx = event.values[0];
-                    vy = event.values[1];
-                    vz = event.values[2];
-
-                    myRender.setRotx(vx);
-                    myRender.setRoty(vy);
-                    myRender.setRotz(vz);
-                    myRender.setmAngle(myRender.getmAngle()+0.4f);
-                }
-                else{
-                    myRender.setRotx(vx);
-                    myRender.setRoty(vy);
-                    myRender.setRotz(vz);
-                }
-
-            }
-
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-            }
-
-        };
-        sensorManager.registerListener(sensorEventListener,sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+                SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Get updates from the accelerometer and magnetometer at a constant rate.
+        // To make batch operations more efficient and reduce power consumption,
+        // provide support for delaying updates to the application.
+        //
+        // In this example, the sensor reporting delay is small enough such that
+        // the application receives an update before the system checks the sensor
+        // readings again.
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+                SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // Don't receive any more updates from either sensor.
+        mSensorManager.unregisterListener(mSensorListener);
+    }
+
+    public void updateOrientationAngles() {
+        mSensorManager.getRotationMatrix(mRotationMatrix, null,
+                mAccelerometerReading, mMagnetometerReading);
+
+        float x = mOrientationAngles[0];
+        float y = mOrientationAngles[1];
+        float z = mOrientationAngles[2];
+
+        mSensorManager.getOrientation(mRotationMatrix, mOrientationAngles);
+
+        myRender.setRotx(mOrientationAngles[0]);
+        myRender.setRoty(mOrientationAngles[1]);
+        myRender.setRotz(mOrientationAngles[2]);
+        float dist = (float)(Math.sqrt(Math.pow(x-mOrientationAngles[0],2)+Math.pow(y-mOrientationAngles[1],2)+Math.pow(z-mOrientationAngles[2],2)));
+        if(Math.abs(dist)>0.001f)
+            myRender.setmAngle(myRender.getmAngle()+(dist*0.5f));
+    }
+
 
 
     private static final float TOUCH_SCALE_FACTOR = 0.0015f;
